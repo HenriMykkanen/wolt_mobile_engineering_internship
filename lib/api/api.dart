ec336a11bf2a574ebd2bca96ba1bd2376582ed1f
HenriMykkanen <144138353+HenriMykkanen@users.noreply.github.com>
2024-01-29 14:24:51 +0200

    API
    API done, not sure if it works yet because need to figure out how to get longitude and latitude parameters and use them in the API call.
    
    Added internet + location data permissions to androidmanifest.xml

---
+++ lib/api/api.dart
@@ -0,0 +1,29 @@
+import 'dart:html';
+
+class WoltAPI {
+  WoltAPI();
+
+  static const String _apiBaseUrl = "restaurant-api.wolt.com";
+  static const String _apiPath = "/v1";
+
+  Uri restaurants(lat, lon) => _buildUri(
+        endpoint: "/pages/restaurants",
+        parametersBuilder: () => restaurantQueryParameters(lat, lon),
+      );
+
+  Uri _buildUri({
+    required String endpoint,
+    required Map<String, dynamic> Function() parametersBuilder,
+  }) {
+    return Uri(
+      scheme: "https",
+      host: _apiBaseUrl,
+      path: "$_apiPath$endpoint",
+      queryParameters: parametersBuilder(),
+    );
+  }
+
+  Map<String, dynamic> restaurantQueryParameters(String lat, String lon) => {
+        "q": lat + lon,
+      };
+}
